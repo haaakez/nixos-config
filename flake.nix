9e5655bf81902da97ab11bea15bcf4074293a53c
haaakez <matviy.zakrevskyi@gmail.com>
2026-04-13 17:22:45 +0300

    add laptop config without nvidia

--- flake.nix
+++ flake.nix
@@ -1,27 +1,54 @@
 {
   description = "NixOS Flake with Noctalia Shell";
-  inputs.spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+
   inputs = {
-    # Noctalia needs the latest dependencies, so we track unstable
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
-    # The official Noctalia GitHub Flake
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, noctalia, ... }@inputs: {
-    # NOTE: "nixos" is your default hostname. If you named your PC 
-    # something else during setup, change "nixos" here to match!
-    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; }; # Passes the flake inputs to your config
-      modules = [
-        ./hardware-configuration.nix
-        ./configuration.nix
-      ];
+  outputs = { self, nixpkgs, noctalia, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+
+      "nixos" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.users.haakez = import ./home.nix;
+          }
+        ];
+      };
+
+      "notnvidia" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./notnvidia/hardware-configuration.nix
+          ./notnvidia/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.users.haakez = import ./home.nix;
+          }
+        ];
+      };
+
     };
   };
 }
