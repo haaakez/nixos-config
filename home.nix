987036e797c0091f57d23607066c594786a5fd09
хейкез <126398394+haaakez@users.noreply.github.com>
2026-04-14 00:46:32 +0300

    Update home.nix

--- home.nix
+++ home.nix
@@ -8,5 +8,5 @@
   home.file.".config/noctalia".source = ./noctalia;
   home.file.".config/kitty".source = ./kitty;
   home.file.".config/spicetify".source = ./spicetify;
-  home.file."Pictures/Wallpapers/wallpaper.png".source = ./wallpaper.png;
+  home.file."Pictures/Wallpapers/wFreeSample-Vectorizer-io-images.steamusercontent(1).png".source = ./wallpaper.png;
 }
