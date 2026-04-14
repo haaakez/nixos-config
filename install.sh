dba2fb77fe7da0bbd9fe4f8ca4f09fb4992aa499
хейкез <126398394+haaakez@users.noreply.github.com>
2026-04-14 20:41:11 +0300

    Update wallpaper copy command in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 HOST=${1:-nixos}
 mkdir -p ~/Pictures/Wallpapers/
-cp -r ~/nixos-config/FreeSample-Vectorizer-io-images.steamusercontent(1).png ~/Pictures/Wallpapers/
+cp -r ~/nixos-config/wallpapers/* ~/Pictures/Wallpapers/
 cp /etc/nixos/hardware-configuration.nix ~/nixos-config/$HOST/hardware-configuration.nix 2>/dev/null || \
 cp /etc/nixos/hardware-configuration.nix ~/nixos-config/hardware-configuration.nix
 sudo nixos-rebuild switch --flake ~/nixos-config#$HOST
